013b7ef31de6c1b7b0a7c9856f3db28e9e1f4f47
vsbreno <brenovinicius.santos0@gmail.com>
2026-03-03 20:07:06 -0300

    try-with-resources first time

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,50 +8,20 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		/*
-		 * File file = new File("D:\\test\\test.txt"); Scanner scan = null;
-		 * 
-		 * try { scan = new Scanner(file); while (scan.hasNextLine()) {
-		 * System.out.println(scan.nextLine()); } } catch (IOException e) {
-		 * System.out.println(e.getMessage()); } finally { if (scan != null) {
-		 * scan.close(); } }
-		 */
-
 		String path = "D:\\test\\test.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
 			String line = br.readLine();
 
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-
 		} 
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} 
-		finally {
-
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 
 }
